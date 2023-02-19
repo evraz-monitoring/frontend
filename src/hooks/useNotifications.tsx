ccb53dbffb9f2939c14efcda05c303db4f1f3f36
borjomeeee <spiridonov.new@gmail.com>
2023-02-19 08:40:03 +0300

    Fix hook

--- src/hooks/useNotifications.tsx
+++ src/hooks/useNotifications.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+import { signalsMapping } from "../lib/SignalsNames";
 import { getNotifications } from "../redux/store/exchausters/selectors";
 import { useAppSelector } from "../redux/utils";
 
 export const useNotifications = () => {
     const notifications = useAppSelector(getNotifications);
-    return { notifications };
+
+    const processed = React.useMemo(
+        () =>
+            notifications.map((notif) => ({
+                exchauster: notif.exhauster,
+                type: notif.type,
+                title: (signalsMapping as any)[notif.exhauster][notif.metric],
+            })),
+        [notifications]
+    );
+    return { notifications: processed };
 };
